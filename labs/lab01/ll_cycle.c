228c4a3007b02a50667927d44f054aaf2c1ee012
painjoker <painjoker42@gmail.com>
2025-01-05 18:29:26 +0800

    ✨ feat: finish lab01
    - only command in gdb.txt is required, but do not have autograder, so.

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -2,6 +2,15 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    
+    while(hare->next && hare->next->next) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
